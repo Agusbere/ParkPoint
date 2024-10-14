a929ba3e3a3a0942d24277b1080a321e9e7357fe
DAVID CHINSKI <48592365@ort>
2024-10-14 10:24:50 -0300

    agregar imagenes y arrancar footery header

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,9 @@
-﻿var map = L.map('map').setView([51.505, -0.09], 13);
+﻿var map = L.map('map').setView([-34.6037, -58.3816], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+    }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+    L.marker([-34.6037, -58.3816]).addTo(map)
+        .bindPopup('Buenos Aires, CABA')
+        .openPopup();
