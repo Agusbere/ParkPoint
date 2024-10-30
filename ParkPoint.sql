3ac5958460c421725589cbf5189267ae39c8eff5
Agusbere <48178406@est.ort.edu.ar>
2024-10-29 21:15:18 -0300

    Cambios en la base de datos

--- ParkPoint.sql
+++ ParkPoint.sql
@@ -4,26 +4,54 @@ GO
 USE [ParkPoint];
 GO
 
+-------------------------------------------TABLAS------------------------------------------------------------
+CREATE TABLE [dbo].[Genero](
+    [id_genero] INT IDENTITY(1,1) PRIMARY KEY,
+    [nombre_genero] VARCHAR(255) NOT NULL
+);
+GO
+
 CREATE TABLE [dbo].[Usuario](
     [id_usuario] INT IDENTITY(1,1) PRIMARY KEY,
     [dni] INT NOT NULL,
     [foto_dni] VARCHAR(255) NOT NULL,
     [nombre] VARCHAR(255) NOT NULL,
     [apellido] VARCHAR(255) NOT NULL,
+    [fecha_nacimiento] DATE NOT NULL,
     [email] VARCHAR(255) NOT NULL,
     [contrasena] VARCHAR(255) NOT NULL,
-    [fecha_registro] TIME(7) NOT NULL,
-    [foto_usuario] VARCHAR(255) NOT NULL
+    [fecha_registro] DATE NOT NULL,
+    [fecha_vencimiento_carnet] DATE NOT NULL,
+    [foto_carnet] VARCHAR(255) NOT NULL,
+    [foto_usuario] VARCHAR(255) NOT NULL,
+    [id_genero] INT NOT NULL
+    FOREIGN KEY ([id_genero]) REFERENCES [dbo].[Genero]([id_genero])
+);
+GO
+
+CREATE TABLE [dbo].[Marca](
+    [id_marca] INT IDENTITY(1,1) PRIMARY KEY,
+    [nombre_marca] VARCHAR(255) NOT NULL
+);
+GO
+
+CREATE TABLE [dbo].[Modelo](
+    [id_modelo] INT IDENTITY(1,1) PRIMARY KEY,
+    [nombre_modelo] VARCHAR(255) NOT NULL,
+    [id_marca] INT NOT NULL
+    FOREIGN KEY ([id_marca]) REFERENCES [dbo].[Marca]([id_marca])
 );
 GO
 
 CREATE TABLE [dbo].[Auto](
     [id_auto] INT IDENTITY(1,1) PRIMARY KEY,
     [patente] VARCHAR(50) NOT NULL,
-    [marca] VARCHAR(255) NOT NULL,
-    [modelo] VARCHAR(255) NOT NULL,
     [id_usuario] INT NOT NULL,
-    FOREIGN KEY ([id_usuario]) REFERENCES [dbo].[Usuario]([id_usuario])
+    [id_marca] INT NOT NULL,
+    [id_modelo] INT NOT NULL
+    FOREIGN KEY ([id_usuario]) REFERENCES [dbo].[Usuario]([id_usuario]),
+    FOREIGN KEY ([id_marca]) REFERENCES [dbo].[Marca]([id_marca]),
+    FOREIGN KEY ([id_modelo]) REFERENCES [dbo].[Modelo]([id_modelo])
 );
 GO
 
@@ -40,7 +68,9 @@ CREATE TABLE [dbo].[Estacionamiento](
     [ubicacionY] FLOAT NOT NULL,
     [tiempo_promedio_llegada] TIME(7) NOT NULL,
     [id_auto] INT NOT NULL,
-    FOREIGN KEY ([id_auto]) REFERENCES [dbo].[Auto]([id_auto])
+    [id_usuario] INT NOT NULL
+    FOREIGN KEY ([id_auto]) REFERENCES [dbo].[Auto]([id_auto]),
+    FOREIGN KEY ([id_usuario]) REFERENCES [dbo].[Usuario]([id_usuario])
 );
 GO
 
@@ -67,7 +97,7 @@ CREATE TABLE [dbo].[Recompensa](
     [id_recompensa] INT IDENTITY(1,1) PRIMARY KEY,
     [titulo] TEXT NOT NULL,
     [descripcion] TEXT NOT NULL,
-    [puntos_necesarias] INT NOT NULL,
+    [puntos_necesarios] INT NOT NULL,
     [imagen] VARCHAR(255) NOT NULL,
     [id_canjeo] INT NOT NULL,
     FOREIGN KEY ([id_canjeo]) REFERENCES [dbo].[Canjeo]([id_canjeo])
@@ -93,16 +123,107 @@ CREATE TABLE [dbo].[Registro](
 );
 GO
 
+CREATE TABLE [dbo].[Tiempo_Real](
+    [id_reporte_tiempo_real] INT IDENTITY(1,1) PRIMARY KEY,
+    [descripcion] VARCHAR(255) NULL,
+    [ubicacionX] FLOAT NOT NULL,
+    [ubicacionY] FLOAT NOT NULL
+);
+GO
+
+CREATE TABLE [dbo].[Infraccion](
+    [id_reporte_infraccion] INT IDENTITY(1,1) PRIMARY KEY,
+    [descripcion] VARCHAR(255) NULL
+);
+GO
+
 CREATE TABLE [dbo].[Reporte](
     [id_reporte] INT IDENTITY(1,1) PRIMARY KEY,
     [enTiempoRealOInfraccion] BIT NOT NULL,
-    [Motivo_tiempo_real] VARCHAR(255) NULL,
-    [Motivo_infraccion] VARCHAR(255) NULL,
     [calle_infraccion] VARCHAR(255) NULL,
     [altura_infraccion] VARCHAR(255) NULL,
     [patente_reportada] VARCHAR(255) NULL,
     [fecha_reporte] DATE NOT NULL,
     [id_usuario] INT NOT NULL,
-    FOREIGN KEY ([id_usuario]) REFERENCES [dbo].[Usuario]([id_usuario])
+    [id_reporte_tiempo_real] INT NOT NULL,
+    [id_reporte_infraccion] INT NOT NULL
+    FOREIGN KEY ([id_usuario]) REFERENCES [dbo].[Usuario]([id_usuario]),
+    FOREIGN KEY ([id_reporte_tiempo_real]) REFERENCES [dbo].[Tiempo_Real]([id_reporte_tiempo_real]),
+    FOREIGN KEY ([id_reporte_infraccion]) REFERENCES [dbo].[Infraccion]([id_reporte_infraccion])
 );
 GO
+---------------------------------------------INSERTS--------------------------------------------------------
+INSERT INTO [dbo].[Genero] (nombre_genero)
+VALUES ('Masculino'), ('Femenino'), ('No binario');
+GO
+
+--Agregar insert de tiempo real
+--Agregar insert de infraccion
+--Agregar insert de marcas de autos
+--Agregar insert de modelos de autos
+
+-----------------------------------------STORED PROCEDURES---------------------------------------------------
+CREATE PROCEDURE SP_Registrarse
+    @DNI INT,
+    @FotoDNI VARCHAR(255),
+    @Nombre VARCHAR(255),
+    @Apellido VARCHAR(255),
+    @Email VARCHAR(255),
+    @Contrasena VARCHAR(255),
+    @FechaNacimiento DATE,
+    @FechaVencimientoCarnet TIME(7),
+    @FotoCarnet VARCHAR(255),
+    @FotoUsuario VARCHAR(255),
+    @IdGenero INT
+AS
+BEGIN
+    DECLARE @Edad INT;
+    DECLARE @AñoActual DATE = GETDATE();
+
+    -- Validación de DNI: No debe tener más de 8 dígitos
+    IF LEN(CAST(@DNI AS VARCHAR(10))) > 8
+    BEGIN
+        RAISERROR('El DNI no puede tener más de 8 dígitos.', 16);
+        RETURN;
+    END;
+
+    -- Validación de Fecha de Vencimiento del Carnet: No puede ser una fecha pasada
+    IF @FechaVencimientoCarnet < CONVERT(TIME, GETDATE())
+    BEGIN
+        RAISERROR('La fecha de vencimiento del carnet no puede ser una fecha pasada.', 16);
+        RETURN;
+    END;
+
+    -- Validación de Edad: Debe ser igual o mayor a 17 años
+    SET @Edad = DATEDIFF(YEAR, @FechaNacimiento, @AñoActual);
+    IF (MONTH(@FechaNacimiento) > MONTH(@AñoActual) OR
+       (MONTH(@FechaNacimiento) = MONTH(@AñoActual) AND DAY(@FechaNacimiento) > DAY(@AñoActual)))
+    BEGIN
+        SET @Edad = @Edad - 1;
+    END;
+
+    IF @Edad < 17
+    BEGIN
+        RAISERROR('La edad debe ser igual o mayor a 17 años.', 16);
+        RETURN;
+    END;
+
+    -- Si todas las validaciones son exitosas, inserta el nuevo usuario en la tabla Usuario
+    INSERT INTO [dbo].[Usuario] (dni, foto_dni, nombre, apellido, email, contrasena, fecha_registro, 
+    fecha_vencimiento_carnet, foto_carnet, fecha_nacimiento, id_genero, foto_usuario)
+    VALUES (@DNI, @FotoDNI, @Nombre, @Apellido, @Email, @Contrasena, CONVERT(TIME, GETDATE()), 
+            @FechaVencimientoCarnet, @FotoCarnet, @FechaNacimiento, @IdGenero, @FotoUsuario);
+END;
+GO
+
+CREATE PROCEDURE SP_IniciarSesion
+@Email VARCHAR(255),
+@Contrasena VARCHAR(255)
+AS
+BEGIN
+SELECT Usuario.*
+FROM Usuario
+WHERE email = @Email AND contrasena = @Contrasena;
+END;
+GO
+
