5bf56d72e16fe27fd5e5220ceb3261c6f4124256
DAVID CHINSKI <48592365@ort>
2024-11-20 09:10:05 -0300

    arrreglar cosas

--- sql/querys.sql
+++ sql/querys.sql
@@ -5,6 +5,50 @@ VALUES
     ('Estacionamiento mal indicado');
 GO
 
+INSERT INTO [dbo].[Marca] (nombre_marca) VALUES
+    ('Ford'), ('Fiat'), ('Peugeot'), ('Citroën'), ('Chevrolet'), 
+    ('Nissan'), ('Renault'), ('Volkswagen'), ('Toyota'), 
+    ('Honda'), ('Hyundai'), ('Kia'), ('Mercedes Benz'), 
+    ('BMW'), ('Audi'), ('Changan'), ('JAC Motors'), ('BYD');
+GO
+
+INSERT INTO [dbo].[Modelo] (nombre_modelo, id_marca) VALUES
+    ('Ford Fiesta', 1), ('Ford Focus', 1), ('Ford EcoSport', 1), 
+    ('Ford Ranger', 1), ('Ford Kuga', 1), ('Ford Mustang', 1),
+    ('Fiat Uno', 2), ('Fiat Palio', 2), ('Fiat Argo', 2), 
+    ('Fiat Cronos', 2), ('Fiat Toro', 2),
+    ('Peugeot 208', 3), ('Peugeot 308', 3), ('Peugeot 2008', 3), 
+    ('Peugeot 3008', 3), ('Peugeot 5008', 3),
+    ('Citroën C3', 4), ('Citroën C4 Lounge', 4), ('Citroën C5 Aircross', 4), 
+    ('Citroën Berlingo', 4),
+    ('Chevrolet Onix', 5), ('Chevrolet Tracker', 5), ('Chevrolet Spin', 5), 
+    ('Chevrolet S10', 5), ('Chevrolet Tracker', 5),
+    ('Nissan Versa', 6), ('Nissan Frontier', 6), ('Nissan Kicks', 6), 
+    ('Nissan Qashqai', 6),
+    ('Renault Kwid', 7), ('Renault Sandero', 7), ('Renault Logan', 7), 
+    ('Renault Duster', 7), ('Renault Oroch', 7),
+    ('Volkswagen Gol', 8), ('Volkswagen Polo', 8), ('Volkswagen T-Cross', 8), 
+    ('Volkswagen Tiguan', 8), ('Volkswagen Amarok', 8),
+    ('Toyota Corolla', 9), ('Toyota Hilux', 9), ('Toyota Yaris', 9), 
+    ('Toyota RAV4', 9), ('Toyota Land Cruiser', 9),
+    ('Honda HR-V', 10), ('Honda Civic', 10), ('Honda City', 10), 
+    ('Honda CR-V', 10),
+    ('Hyundai Creta', 11), ('Hyundai Tucson', 11), ('Hyundai Elantra', 11), 
+    ('Hyundai Santa Fe', 11),
+    ('Kia Seltos', 12), ('Kia Sportage', 12), ('Kia Rio', 12), 
+    ('Kia Cerato', 12),
+    ('Mercedes-Benz Clase A', 13), ('Mercedes-Benz Clase C', 13), 
+    ('Mercedes-Benz GLA', 13), ('Mercedes-Benz GLC', 13),
+    ('BMW Serie 1', 14), ('BMW Serie 3', 14), ('BMW X1', 14), 
+    ('BMW X3', 14),
+    ('Audi A3', 15), ('Audi A4', 15), ('Audi Q3', 15), 
+    ('Audi Q5', 15),
+    ('Changan CS35', 16), ('Changan Alsvin', 16),
+    ('JAC S2', 17), ('JAC S3', 17), ('JAC T6', 17),
+    ('BYD F0', 18), ('BYD Song', 18);
+GO
+
+
 INSERT INTO [dbo].[Usuario] (dni, foto_dni, nombre, apellido, email, contrasena)
 VALUES 
     (12345678, 'foto_dni_usuario.jpg', 'Juan', 'Pérez', 'juan.perez@mail.com', 'password123');
@@ -70,49 +114,6 @@ VALUES
 GO
 
 
-
-INSERT INTO [dbo].[Marca] (nombre_marca) VALUES
-    ('Ford'), ('Fiat'), ('Peugeot'), ('Citroën'), ('Chevrolet'), 
-    ('Nissan'), ('Renault'), ('Volkswagen'), ('Toyota'), 
-    ('Honda'), ('Hyundai'), ('Kia'), ('Mercedes Benz'), 
-    ('BMW'), ('Audi'), ('Changan'), ('JAC Motors'), ('BYD');
-GO
-
-INSERT INTO [dbo].[Modelo] (nombre_modelo, id_marca) VALUES
-    ('Ford Fiesta', 1), ('Ford Focus', 1), ('Ford EcoSport', 1), 
-    ('Ford Ranger', 1), ('Ford Kuga', 1), ('Ford Mustang', 1),
-    ('Fiat Uno', 2), ('Fiat Palio', 2), ('Fiat Argo', 2), 
-    ('Fiat Cronos', 2), ('Fiat Toro', 2),
-    ('Peugeot 208', 3), ('Peugeot 308', 3), ('Peugeot 2008', 3), 
-    ('Peugeot 3008', 3), ('Peugeot 5008', 3),
-    ('Citroën C3', 4), ('Citroën C4 Lounge', 4), ('Citroën C5 Aircross', 4), 
-    ('Citroën Berlingo', 4),
-    ('Chevrolet Onix', 5), ('Chevrolet Tracker', 5), ('Chevrolet Spin', 5), 
-    ('Chevrolet S10', 5), ('Chevrolet Tracker', 5),
-    ('Nissan Versa', 6), ('Nissan Frontier', 6), ('Nissan Kicks', 6), 
-    ('Nissan Qashqai', 6),
-    ('Renault Kwid', 7), ('Renault Sandero', 7), ('Renault Logan', 7), 
-    ('Renault Duster', 7), ('Renault Oroch', 7),
-    ('Volkswagen Gol', 8), ('Volkswagen Polo', 8), ('Volkswagen T-Cross', 8), 
-    ('Volkswagen Tiguan', 8), ('Volkswagen Amarok', 8),
-    ('Toyota Corolla', 9), ('Toyota Hilux', 9), ('Toyota Yaris', 9), 
-    ('Toyota RAV4', 9), ('Toyota Land Cruiser', 9),
-    ('Honda HR-V', 10), ('Honda Civic', 10), ('Honda City', 10), 
-    ('Honda CR-V', 10),
-    ('Hyundai Creta', 11), ('Hyundai Tucson', 11), ('Hyundai Elantra', 11), 
-    ('Hyundai Santa Fe', 11),
-    ('Kia Seltos', 12), ('Kia Sportage', 12), ('Kia Rio', 12), 
-    ('Kia Cerato', 12),
-    ('Mercedes-Benz Clase A', 13), ('Mercedes-Benz Clase C', 13), 
-    ('Mercedes-Benz GLA', 13), ('Mercedes-Benz GLC', 13),
-    ('BMW Serie 1', 14), ('BMW Serie 3', 14), ('BMW X1', 14), 
-    ('BMW X3', 14),
-    ('Audi A3', 15), ('Audi A4', 15), ('Audi Q3', 15), 
-    ('Audi Q5', 15),
-    ('Changan CS35', 16), ('Changan Alsvin', 16),
-    ('JAC S2', 17), ('JAC S3', 17), ('JAC T6', 17),
-    ('BYD F0', 18), ('BYD Song', 18);
-GO
 
 
 -----------------------------------------STORED PROCEDURES---------------------------------------------------
@@ -203,6 +204,6 @@ BEGIN
     BEGIN
         -- Insertar un nuevo espacio de estacionamiento en la ubicación especificada
         INSERT INTO Estacionamiento (ocupado, calle, altura_calle, fecha_ocupado, id_auto, ubicacionX, ubicacionY)
-        VALUES (1, @Calle, @Altura, GETDATE(), 1, @UbicacionX, @UbicacionY);
+        VALUES (1, @Calle, @Altura, GETDATE(), @IdAuto, @UbicacionX, @UbicacionY);
     END
 END;
