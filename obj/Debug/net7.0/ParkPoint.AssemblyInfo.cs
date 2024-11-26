e65da23226e431d7988cd57fdd981bba26341e11
DAVID CHINSKI <48592365@ort>
2024-11-26 13:14:24 -0300

    que aparezca el mapa

--- obj/Debug/net7.0/ParkPoint.AssemblyInfo.cs
+++ obj/Debug/net7.0/ParkPoint.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParkPoint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ab07fb1e66372698a7a7a56d98de1930b806145")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2136e5a48e965875a8e6c08e8f487620740cc655")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParkPoint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParkPoint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
