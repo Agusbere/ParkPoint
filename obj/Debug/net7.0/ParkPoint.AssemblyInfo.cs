2f10e9d611d3c98ee7fef0cd7dc962c2ffe0c904
Agusbere <48178406@est.ort.edu.ar>
2024-12-04 00:26:12 -0300

    estilo del boton desocupar espacio

--- obj/Debug/net7.0/ParkPoint.AssemblyInfo.cs
+++ obj/Debug/net7.0/ParkPoint.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParkPoint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f74e03ffcf8eb987cb941ae23d594c6d661bb41b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d26b5acbdc0f282758f8706e8a9e7f1af751cddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParkPoint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParkPoint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
